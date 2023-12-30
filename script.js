ebb6e5715d7e2b99933a2161e424058cba9d6d2a
Pedro Henrique Barbosa Carvalho <pedrohbc1504@gmail.com>
2023-12-30 15:04:48 -0300

    test commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function testFunc(){
+    window.location.href = "https://www.google.com/search?client=opera-gx&q=josé+porras&sourceid=opera&ie=UTF-8&oe=UTF-8";
+}
+
+function verifyFunc(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var userYear = document.getElementById("txtyear");
+    var ans = document.getElementById("ans");
+
+    if (userYear.value.length == 0 || parseFloat(userYear.value) > year){
+        window.alert(`[ERROR] Verify the inserted data and try again.`);
+    }
+    else {
+        var userGender = document.getElementsByName('radgender');
+        var age = year - parseInt(userYear.value);
+        var gender = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'picture');
+        if (userGender[0].checked){
+            gender = 'male';
+            if (age >= 0 && age < 40){
+                img.setAttribute('src', 'test.jpg');
+            }
+            if (age >= 40){
+                img.setAttribute('src', 'test.jpg');
+            }
+        }
+        else if (userGender[1].checked){
+            gender = 'female';
+            if (age >= 0 && age < 40){
+                img.setAttribute('src', 'test.jpg');
+            }
+            if (age >= 40){
+                img.setAttribute('src', 'test.jpg');
+            }
+        }
+
+        ans.style.textAlign = 'center';
+        ans.innerHTML = `Detected a ${age} years old ${gender}`;
+        ans.appendChild(img);
+    }
+}
